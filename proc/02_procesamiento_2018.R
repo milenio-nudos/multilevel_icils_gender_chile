d33bde1f3529281ec3e8b7991f42226ac6f02cb9
Nicolas Tobar <nicolas.tobar@ug.uchile.cl>
2024-01-17 18:09:58 -0300

    script paper women x women

--- proc/02_procesamiento_2018.R
+++ proc/02_procesamiento_2018.R
@@ -24,10 +24,18 @@ bsgchli2 <- bsgchli2%>%clean_names() #Nombres en minúscula
 btgchli2 <- btgchli2%>%clean_names()
 bcgchli2 <- bcgchli2%>%clean_names()
 
-student_data <- bsgchli2 %>% select (#Demográficas/ID
+
+
+student_data <- bsgchli2 %>% 
+  mutate(i27b=4-is2g27b,
+         i27e=4-is2g27e,
+         i27g=4-is2g27g,
+         i27h=4-is2g27h)%>%
+  mutate(s_speceff2=(i27b+i27e+i27g+i27h)/4)%>%
+                                 select (#Demográficas/ID
                                          idschool, idstud, s_sex,
                                          #Self-efficacy
-                                         s_speceff, s_geneff,
+                                         s_speceff, s_geneff,s_speceff2,
                                          #Demographics
                                          s_hisced,s_homlit,
                                          #Attitudes
@@ -39,39 +47,48 @@ student_data <- bsgchli2 %>% select (#Demográficas/ID
                                          s_genclass, s_speclass,
                                          s_ictlrn, s_codlrn,
                                          #Puntaje prueba
-                                         s_pv1cil=pv1cil)
+                                         s_pv1cil=pv1cil,
+                                         #Specialized self-efficacy items
+                                         is2g27b,is2g27e,is2g27g,is2g27h,
+                                         #Specialized recoded self-efficacy
+                                         i27b,i27e,i27g,i27h,
+                                         #Weights
+                                         totwgts,
+                                         wgtadj1s,wgtfac3s,wgtadj3s
+                                         )
 
-#Agregación de variables nivel 1 a 2
+#Agregación de variables nivel 1 a 2 con factores incluidos
 student_data <- student_data %>% group_by(idschool) %>% 
-  mutate(c_speceff=round(mean(s_speceff, na.rm=TRUE),2), 
-         c_geneff=round(mean(s_geneff, na.rm=TRUE),2),
-         c_ictneg=round(mean(s_ictneg, na.rm=TRUE),2),
-         c_ictpos=round(mean(s_ictpos, na.rm=TRUE),2),
-         c_ictfut=round(mean(s_ictfut, na.rm=TRUE),2),
-         c_usecom=round(mean(s_usecom, na.rm=TRUE),2),
-         c_useinf=round(mean(s_useinf, na.rm=TRUE),2),
-         c_accont=round(mean(s_accont, na.rm=TRUE),2),
-         c_usestd=round(mean(s_usestd, na.rm=TRUE),2),
-         c_ictlrn=round(mean(s_ictlrn, na.rm=TRUE),2),
-         c_speclass=round(mean(s_speclass, na.rm=TRUE),2),
-         c_genclass=round(mean(s_genclass, na.rm=TRUE),2),
-         c_codlrn=round(mean(s_codlrn, na.rm=TRUE),2),
-         c_pv1cil=round(mean(s_pv1cil, na.rm=TRUE),2),
+  mutate(c_speceff=round(weighted.mean(s_speceff, w=wgtadj3s, na.rm=TRUE),2), 
+         c_geneff=round(weighted.mean(s_geneff, w=wgtadj3s, na.rm=TRUE),2),
+         c_ictneg=round(weighted.mean(s_ictneg, w=wgtadj3s, na.rm=TRUE),2),
+         c_ictpos=round(weighted.mean(s_ictpos, w=wgtadj3s, na.rm=TRUE),2),
+         c_ictfut=round(weighted.mean(s_ictfut, w=wgtadj3s, na.rm=TRUE),2),
+         c_usecom=round(weighted.mean(s_usecom, w=wgtadj3s, na.rm=TRUE),2),
+         c_useinf=round(weighted.mean(s_useinf, w=wgtadj3s, na.rm=TRUE),2),
+         c_accont=round(weighted.mean(s_accont, w=wgtadj3s, na.rm=TRUE),2),
+         c_usestd=round(weighted.mean(s_usestd, w=wgtadj3s, na.rm=TRUE),2),
+         c_ictlrn=round(weighted.mean(s_ictlrn, w=wgtadj3s, na.rm=TRUE),2),
+         c_speclass=round(weighted.mean(s_speclass, w=wgtadj3s, na.rm=TRUE),2),
+         c_genclass=round(weighted.mean(s_genclass, w=wgtadj3s, na.rm=TRUE),2),
+         c_codlrn=round(weighted.mean(s_codlrn, w=wgtadj3s, na.rm=TRUE),2),
+         c_pv1cil=round(weighted.mean(s_pv1cil, w=wgtadj3s, na.rm=TRUE),2),
          )
 
 # La base pública de 2018 no contiene la proporción de hombres y mujeres por escuela.
 # Se Estima la proporción de mujeres en 8vo básico, considerando la representatividad de la muestra.
 student_data <- student_data %>% group_by(idschool) %>% 
-                mutate(c_f_ratio_grade= sum(s_sex,na.rm = T),
-                       c_f_ratio_grade=c_f_ratio_grade/n(),
-                       c_f_ratio_grade=round(c_f_ratio_grade,2)) %>%
+                mutate(c_s_f_ratio= sum(s_sex,na.rm = T),
+                       c_s_f_ratio=c_s_f_ratio/n(),
+                       c_s_f_ratio=round(c_s_f_ratio,2)) %>%
                 ungroup()
 
 #2. Preparación base de segundo nivel -----
 
 # Obtener sexo del director.
 school_data<- bcgchli2 %>%  
-  select(idschool, p_sex) %>% #Seleccionar variables a fundir con base de estudiantes
+  select(idschool, p_sex,
+         totwgtc,wgtfac1,wgtadj1c) %>% #Seleccionar variables a fundir con base de estudiantes
   rename(c_p_sex = p_sex) #Rename to school level
 
 #Unir ambas bases de datos a partir de id de escuelas
@@ -89,36 +106,48 @@ label(base[["c_speclass"]]) <- "School mean specialist application ICT in class"
 label(base[["c_genclass"]]) <- "School mean general applications ICT in class"
 label(base[["c_codlrn"]]) <- "School mean Learning coding in class"
 label(base[["c_pv1cil"]]) <- "School mean score CIL Test"
-label(base[["c_f_ratio_grade"]]) <- "School proportion of females students in eigth grade"
+label(base[["c_s_f_ratio"]]) <- "School proportion of females students in eigth grade"
 
 
-#3. Agregar datos de profesores ----
+#3. Agregar datos agregados de profesores ----
 teacher_data <- btgchli2 %>% clean_names() %>% #Nombres en minúscula
   select(idschool,idteach,
-         t_sex, #Género profe (Not representative)
+         t_sex, #Género profe
          t_icteff,
          t_vwpos,
-         t_vwneg) %>%
+         t_vwneg,
+         #Weights
+         wgtadj1t,wgtadj2t,wgtfac1,
+         wgtfac2t,wgtfac3t) %>%
   group_by(idschool) %>%
-  mutate(c_t_eff = round(mean(t_icteff,na.rm=T),2),
-         c_t_vwpos = round(mean(t_vwpos,na.rm=T),2),
-         c_t_vwneg = round(mean(t_vwneg,na.rm=T),2))%>%
+  mutate(c_t_eff = round(weighted.mean(t_icteff,w=wgtfac3t,na.rm=T),2),
+         c_t_vwpos = round(weighted.mean(t_vwpos,w=wgtfac3t,na.rm=T),2),
+         c_t_vwneg = round(weighted.mean(t_vwneg,w=wgtfac3t,na.rm=T),2))%>%
+  mutate(c_t_f_ratio= sum(t_sex,na.rm = T),
+         c_t_f_ratio=c_t_f_ratio/n(),
+         c_t_f_ratio=round(c_t_f_ratio,2)) %>%
   ungroup()
 
 #Merge mean by school teacher variables with students database
 base <- merge(base,
               teacher_data%>%group_by(idschool)%>%
-                summarise_at(vars(c_t_eff,c_t_vwpos,c_t_vwneg),mean),
+              summarise_at(vars(c_t_eff,c_t_vwpos,c_t_vwneg,c_t_f_ratio),mean),
               by="idschool",all.x = TRUE, all.y = FALSE)
 
 #Label new variables
 label(base[["c_t_eff"]]) <- "School mean teacher ICT self-efficacy"
 label(base[["c_t_vwpos"]]) <- "School mean teacher positive ICT views"
 label(base[["c_t_vwneg"]]) <- "School mean teacher negative ICT views"
-
+label(base[["c_t_f_ratio"]]) <- "School female gender teacher ratio"
 
 # Merge school data and teacher data
 base_2 <- merge(school_data, teacher_data, by = "idschool", all.y = TRUE, all.x=FALSE)
+
+#Label new variables
+label(base_2[["c_t_eff"]]) <- "School mean teacher ICT self-efficacy"
+label(base_2[["c_t_vwpos"]]) <- "School mean teacher positive ICT views"
+label(base_2[["c_t_vwneg"]]) <- "School mean teacher negative ICT views"
+label(base_2[["c_t_f_ratio"]]) <- "School female gender teacher ratio"
 
 #Guardar base de datos estudiante-colegio
 saveRDS(base,file="input/proc_data/03_student_proc_2018.rds")
---
+++ proc/04_speceff_validation.R
@@ -0,0 +1,72 @@
+pacman::p_load(tidyverse,
+               gtsummary,
+               sjmisc, #Label manipulation
+               labelled, #Label manipulation
+               summarytools,
+               sjPlot,
+               effects
+               )
+
+student_proc_2018 <- readRDS("input/proc_data/03_student_proc_2018.rds")
+
+base<- student_proc_2018 |> 
+  select(s_sex,
+         s_speceff,s_pv1cil,
+         is2g27b,is2g27e,is2g27g,is2g27h) |>
+  na.omit()
+
+#Negative Correlation speceff wit cil, but positive items with cil
+
+cor(base) #why?
+
+#Delete missing data labels
+val_label(base$s_sex,8) <- NULL
+val_label(base$s_sex,9) <- NULL
+val_label(base$is2g27b,8) <- NULL
+val_label(base$is2g27b,9) <- NULL
+val_label(base$is2g27e,8) <- NULL
+val_label(base$is2g27e,9) <- NULL
+val_label(base$is2g27g,8) <- NULL
+val_label(base$is2g27g,9) <- NULL
+val_label(base$is2g27h,8) <- NULL
+val_label(base$is2g27h,9) <- NULL
+
+#Descriptive Specialized Self-efficacy items
+base |>
+  mutate(s_sex=to_label(s_sex),
+         is2g27b=to_label(is2g27b),
+         is2g27e=to_label(is2g27e),
+         is2g27g=to_label(is2g27g),
+         is2g27h=to_label(is2g27h)) |>
+  dfSummary() |> 
+  view()
+
+#Labels values are in the contrary direction
+
+#Change direction of values
+base <-base |> 
+       mutate_at(vars(starts_with("is2")),
+                 ~4-.)
+
+#Test correlation (Now is positive between items and index, and negativa between items and cil)
+cor(base)
+
+#What happens if we create manually the index?
+base <- base |>
+         mutate(s_speceff2=rowMeans(
+          base[,4:7]
+          ))
+
+cor(base) #Practically is the same index (0.98 alpha)
+  
+
+# Fast plot model
+base <- base |> 
+         mutate(s_sex=to_label(s_sex))
+
+ggplot(base, aes(x=s_pv1cil, y=s_speceff,color=s_sex)) + 
+  geom_point()+
+  geom_smooth(method=lm)
+
+# At more cil, gender efficacy differences increase
+
